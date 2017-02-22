793bb678df83ba4fd1bb08da862cb4b65d99a211
Riccardo Bellingeri <rbellingeri@gilt.com>
2017-02-22 23:07:46 +0000

    Sets a fixed height on the spacer element, to avoid jumps in the page while scrolling

--- src/javascript/page.js
+++ src/javascript/page.js
@@ -7,10 +7,10 @@ const THRESHOLD = 1.0;
 (() => {
   const nav = document.querySelector('.nav');
   const spacer = document.querySelector('.nav-spacer');
+  const aboveTheFoldThreshold = 50;
 
   (() => {
     // https://jeremenichelli.github.io/2016/04/quick-introduction-to-the-intersection-observer-api/
-    IntersectionObserver.prototype.POLL_INTERVAL = 100;
     const onChange = (changes) => {
       const status = changes[0].intersectionRatio < THRESHOLD;
       nav.classList[(status) ? 'add' : 'remove']("nav--collapsed");
@@ -19,14 +19,15 @@ const THRESHOLD = 1.0;
     let observer = new IntersectionObserver(onChange, {
       threshold: THRESHOLD, //100%
       root: null,
+      rootMargin: `${aboveTheFoldThreshold}px`
     });
     observer.observe(spacer);
   })();
 
   (() => {
     let prevY = window.pageYOffset;
-    let pollCycle = 500;
-    let deltaScroll = 120; // The delta needs to be at least as tall as the nav to avoid annoying behaviors.
+    const pollCycle = 500;
+    const deltaScroll = 120; // The scroll position delta needs to be at least as tall as the nav to avoid annoying behaviors.
 
     const detectScroll = throttle(() => {
       let curY = window.pageYOffset;
@@ -36,8 +37,9 @@ const THRESHOLD = 1.0;
         nav.classList.remove("nav--collapsed");
         spacer.classList.remove("nav-spacer--collapsed");
       }
-      else if (curY > prevY + 10) {
-        //user is scrolling down by more than 10px
+      else if (curY > prevY + 10 && curY > aboveTheFoldThreshold) {
+        //user is scrolling down by more than 10px.
+        //The second condition is to ensure that the user has scrolled at least `aboveTheFoldThreshold` into the above-the-fold area
         console.log('scrollDown');
         nav.classList.add("nav--collapsed");
         spacer.classList.add("nav-spacer--collapsed");
